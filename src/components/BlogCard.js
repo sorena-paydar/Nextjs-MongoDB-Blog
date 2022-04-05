8f111eccf674d31365c1272aa7a5d3245df678d1
sorena <sorenapaydar81@gmail.com>
2022-04-05 20:54:54 +0430

    add delete each blog feat

--- src/components/BlogCard.js
+++ src/components/BlogCard.js
@@ -1,6 +1,7 @@
 import styles from "../../styles/BlogCard.module.css";
 import timeElapsed from "../../utils/timeElapsed";
 import { httpDeleteBlogById } from "../hooks/requests";
+import useStore from "../zustand/store";
 
 const BlogCard = ({ blog }) => {
   const { blogId, author, content, date } = blog;
@@ -13,25 +14,31 @@ const BlogCard = ({ blog }) => {
 
   const timeDiff = timeElapsed(timestamp);
 
+  const deleteBlog = useStore((state) => state.deleteBlog);
+
   const deleteHandler = async () => {
     const res = await httpDeleteBlogById(blogId);
+    deleteBlog(blogId);
     console.log(res);
   };
 
   return (
     <li className={styles.blogCard}>
-      <div className={styles.blogDetails}>
-        <span className={styles.author}>{author}</span>
-        <span className={styles.date}>
-          {localDate} · {timeDiff}
-        </span>
-      </div>
+      <div className={styles.header}>
+        <div>
+          <span className={styles.author}>{author}</span>
+          <span className={styles.date}>
+            {localDate} • {timeDiff}
+          </span>
+        </div>
 
-      <div className={styles.blogContainer}>
-        <span className={styles.content}>{content}</span>
         <button className={styles.deleteButton} onClick={deleteHandler}>
           DELETE
         </button>
+      </div>
+
+      <div className={styles.blogContent}>
+        <span className={styles.content}>{content}</span>
       </div>
     </li>
   );
