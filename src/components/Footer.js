4cdc7c6e1946b6ddd7ac0b6e9a730b10c722a6b8
sorena <sorenapaydar81@gmail.com>
2022-04-06 18:52:11 +0430

    add copyright

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import styles from "../../styles/Footer.module.css";
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <span className={styles.copyright}>Copyright © 2022</span>
+    </footer>
+  );
+};
+
+export default Footer;
