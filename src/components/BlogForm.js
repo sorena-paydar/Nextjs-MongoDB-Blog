b733f673d403aa263107a260e125139f0ebf15f7
sorena <sorenapaydar81@gmail.com>
2022-04-06 18:55:13 +0430

    add validation to blog form

--- src/components/BlogForm.js
+++ src/components/BlogForm.js
@@ -1,13 +1,13 @@
-import { Form, Input, SubmitButton } from "formik-antd";
+import { Form, FormItem, Input, SubmitButton } from "formik-antd";
 import { Formik } from "formik";
 import { httpAddNewBlog } from "../hooks/requests";
 import styles from "../../styles/BlogForm.module.css";
 import useStore from "../zustand/store";
+import * as Yup from "yup";
 
-const styleProps = {
-  style: {
-    marginBottom: 10,
-  },
+const props = {
+  showCount: true,
+  size: "large",
 };
 
 const BlogForm = () => {
@@ -18,19 +18,50 @@ const BlogForm = () => {
     resetForm();
   };
 
+  const validationSchema = Yup.object().shape({
+    author: Yup.string()
+      .min(4, "Too short")
+      .max(18, "Too Long")
+      .required("Required"),
+    content: Yup.string().max(800, "Message too long").required("Required"),
+  });
+
   return (
     <div className={styles.blogForm}>
-      <Formik initialValues={{}} onSubmit={onSubmit}>
+      <Formik
+        initialValues={{}}
+        onSubmit={onSubmit}
+        validationSchema={validationSchema}
+      >
         {() => (
-          <Form>
-            <Input name="author" placeholder="Name" {...styleProps} />
-            <Input.TextArea
-              name="content"
-              placeholder="Message"
-              rows={5}
-              {...styleProps}
-            />
-            <SubmitButton>SEND</SubmitButton>
+          <Form style={{ width: 500 }}>
+            <FormItem name="author">
+              <Input
+                name="author"
+                placeholder="Name"
+                maxLength={18}
+                {...props}
+                className={styles.authorInput}
+                suffix={<span />}
+              />
+            </FormItem>
+
+            <FormItem name="content">
+              <Input.TextArea
+                name="content"
+                placeholder="Message"
+                autoSize={{ minRows: 5 }}
+                maxLength={800}
+                {...props}
+                className={styles.contentTextarea}
+                suffix={<span />}
+              />
+            </FormItem>
+            <div className={styles.sendButtonWrapper}>
+              <SubmitButton shape="round" size="large">
+                SEND
+              </SubmitButton>
+            </div>
           </Form>
         )}
       </Formik>
