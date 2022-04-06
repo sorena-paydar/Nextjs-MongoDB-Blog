ad3fe3d5d1d5483382dc3df229883fde1409fcec
sorena <sorenapaydar81@gmail.com>
2022-04-06 18:53:21 +0430

    update layout

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,15 @@
-import styles from "../styles/Home.module.css";
 import BlogForm from "../src/components/BlogForm";
 import "antd/dist/antd.css";
 import BlogsGrid from "../src/components/BlogsGrid";
+import Layout from "../src/components/Layout";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <BlogForm />
-      <BlogsGrid />
-    </div>
+    <Layout>
+      <div>
+        <BlogForm />
+        <BlogsGrid />
+      </div>
+    </Layout>
   );
 }
